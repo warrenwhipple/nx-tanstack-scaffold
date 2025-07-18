420f5df1ef395a9be178a7407f059750f1a02b83
Warren Whipple <w@warrenwhipple.com>
2025-07-18 13:02:08 +0100

    feat(web): switch to tanstack start

--- apps/web-e2e/src/example.spec.ts
+++ apps/web-e2e/src/example.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('shows welcome text', async ({ page }) => {
   await page.goto('/');
-
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  expect(await page.locator('h3').innerText()).toContain('Welcome Home');
 });
